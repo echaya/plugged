97e14fe633e1a546c989eb7038349a6ceb4f6a51
Frederick Z. <echaya@gmail.com>
2024-07-22 07:23:03 +0800

    mvoe delGit-test to delGitV3

--- delGit-test.bat
+++
@@ -1,4 +0,0 @@
-for /d /r . %%d in (.git) do @if exist "%%d" @if %%~pd NEQ '\temp\' rd /s/q "%%d"
-for /d /r . %%d in (.github) do @if exist "%%d" rd /s/q "%%d"
-:: for /d /r "D:\temp" %d in (.git) do @if exist "%d" echo "%d"
-cmd /k
