34e6b20a5170957a2a29a9269e9d9eb957e522dc
Frederick Z. <echaya@gmail.com>
2024-07-22 16:00:07 +0800

    auto move files from nvim folder

--- delGitV3.bat
+++ delGitV3.bat
@@ -1,3 +1,10 @@
+
+for /d /r . %%d in (plugged) do @if exist "%%d" rd /s/q "%%d"
+
+SET MoveToDir=D:\PluggedRepo
+SET MoveFromDir=d:\Dropbox\neovim\nvim-win64\share\nvim\vimfiles
+ROBOCOPY %MoveFromDir% %MoveToDir% /S
+
 for /d /r . %%d in (.git) do if exist "%%d" if "%%~pd" NEQ "\PluggedRepo\" rd /s/q "%%d"
 for /d /r . %%d in (.github) do if exist "%%d" rd /s/q "%%d"
 lazygit
