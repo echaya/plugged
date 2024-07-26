69d6627d8ca47561d166ea0a7568699b1808e547
Frederick Z. <echaya@gmail.com>
2024-07-26 15:55:37 +0800

    update delgitv3 path
    move under workspace

--- delGitV3.bat
+++ delGitV3.bat
@@ -1,14 +1,15 @@
 @echo on
 for /d /r . %%d in (plugged) do @if exist "%%d" rd /s/q "%%d"
 
-SET MoveToDir=D:\PluggedRepo
+SET MoveToDir=D:\Workspace\PluggedRepo
 SET MoveFromDir=d:\Dropbox\neovim\nvim-win64\share\nvim\vimfiles
 ROBOCOPY %MoveFromDir% %MoveToDir% /S
 
-for /d /r . %%d in (.git) do if exist "%%d" if "%%~pd" NEQ "\PluggedRepo\" rd /s/q "%%d"
+for /d /r . %%d in (.git) do if exist "%%d" if "%%~pd" NEQ "\Workspace\PluggedRepo\" rd /s/q "%%d"
 for /d /r . %%d in (.github) do if exist "%%d" rd /s/q "%%d"
 
-del d:\PluggedRepo\plugged\nvim-treesitter\parser-info\.gitignore
-del d:\PluggedRepo\plugged\nvim-treesitter\parser\.gitignore
+del d:\Workspace\PluggedRepo\plugged\nvim-treesitter\parser-info\.gitignore
+del d:\Workspace\PluggedRepo\plugged\nvim-treesitter\parser\.gitignore
 
 lazygit
+:: cmd /k
