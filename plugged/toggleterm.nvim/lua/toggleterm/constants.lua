5e9e7f36142b84a7ba0fabe4be76ec1ef811d6db
Frederick Z. <echaya@gmail.com>
2024-07-28 11:21:34 +0800

    plug update

---
+++ plugged/toggleterm.nvim/lua/toggleterm/constants.lua
@@ -0,0 +1,12 @@
+local M = {}
+-----------------------------------------------------------
+-- Constants
+-----------------------------------------------------------
+M.FILETYPE = "toggleterm"
+-- -30 and -3 is a magic number based on manual testing of what looks good
+M.shading_amount = -30
+M.shading_ratio = -3
+-- Highlight group name prefix
+M.highlight_group_name_prefix = "ToggleTerm"
+
+return M
