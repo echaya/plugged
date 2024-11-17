25aeb5f35fd84f32472b25a9fad32ec3a92542ac
Frederick Z. <echaya@gmail.com>
2024-11-17 10:43:17 +0800

    does rebocopy

--- CopyBin.bat
+++ CopyBin.bat
@@ -1,6 +1,3 @@
 @echo
-mkdir plugged\markdown-preview.nvim\app\bin
-copy plugged-old\markdown-preview.nvim\app\bin\markdown-preview-win.exe plugged\markdown-preview.nvim\app\bin
-mkdir plugged\telescope-fzf-native.nvim\build
-copy plugged-old\telescope-fzf-native.nvim\build\libfzf.dll plugged\telescope-fzf-native.nvim\build
+robocopy plugged-old\.git plugged\.git /S
 cmd /k
